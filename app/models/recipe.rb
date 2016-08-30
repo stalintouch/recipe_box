97643dc1a7c67286e55e1c1f883a9c581560ab22
stalintouch <stalintouch1983@gmail.com>
2016-08-30 10:44:11 -0400

    Add users, finish style

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,7 @@ class Recipe < ActiveRecord::Base
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :ingredients
   has_many :directions
+  belongs_to :user
 
   accepts_nested_attributes_for :ingredients,
                                 reject_if: proc { |attributes| attributes['name'].blank? },
